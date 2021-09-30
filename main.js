d36c7d2f2115d725ab3e4e3f13168bfbf7aaa78c
BSAdhithya <91680658+BSAdhithya@users.noreply.github.com>
2021-09-30 07:44:16 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+
+function changecolor()
+{
+   var element;
+   element = document.getElementById("heading"); 
+element.style.color="";
+   if(element.style.color==="black"){
+       element.style.color="blue";
+   }
+else if(){
+    
+}
+   else{
+    element.style.color="black";
+}
+}
